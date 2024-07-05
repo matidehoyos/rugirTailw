981a9409aa671d46799e1eacd21841ae4c585ae9
Matias <mati.dehoyosmdp@gmail.com>
2024-07-05 16:21:37 -0300

    }

--- rugir-main/client/src/components/header/Header.jsx
+++ rugir-main/client/src/components/header/Header.jsx
@@ -2,8 +2,31 @@ import { FaWhatsapp } from 'react-icons/fa'
 import Imagenes from '../imagenes/Imagenes'
 import style from './Header.module.css'
 import Alfombras from '../alfombras/Alfombras'
+import { useEffect } from 'react';
 
 function Header() {
+
+    useEffect(() => {
+        const handleScroll = () => {
+          const regalo = document.querySelector('.regalo');
+          const regaloRect = regalo.getBoundingClientRect();
+          const windowHeight = window.innerHeight;
+    
+          if (regaloRect.top < windowHeight) {
+            regalo.classList.add(style.visible);
+          } else {
+            regalo.classList.remove(style.visible);
+          }
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+
+
+
     return (
         <div className={style.container}>
             <button className={style.flotante}><a href="https://api.whatsapp.com/send?phone=542236351363" target="_blank" rel="noopener noreferrer">Hacenos tu consulta <FaWhatsapp className={style.what}/></a></button>
@@ -25,7 +48,7 @@ function Header() {
                     </div>
                 </div>
                 <div className={style.regalo}>
-                    <h4>EL REGALO PERFECTO <br/>NO EXIS...</h4>
+                    <h4 className={style.text}>EL REGALO PERFECTO <br/>NO EXIS...</h4>
                     <img src="./pika.png" alt="alfombra pikachu" />
                 </div>
             </div>
