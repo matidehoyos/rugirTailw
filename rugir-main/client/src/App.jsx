fcf0e5cf2c0d4894cafa72fd75464ed1f8774e84
Matias <mati.dehoyosmdp@gmail.com>
2024-07-06 19:28:16 -0300

    }

--- rugir-main/client/src/App.jsx
+++ rugir-main/client/src/App.jsx
@@ -5,7 +5,6 @@ import Contact from './components/contact/Contact'
 import Encargo from './components/encargo/Encargo'
 import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
-import NavBar from './components/navBar/NavBar'
 import PreguntasFrecuentes from './components/preguntasFrec/PreguntasFrec'
 import Ubicacion from './components/ubicacion/Ubicacion'
 
@@ -14,7 +13,6 @@ function App() {
 
   return (
     <>
-      <NavBar />
       <Header />
       <Alfombras2 />
       <Encargo />
