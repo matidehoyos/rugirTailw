6c25a6ff5585e3890034408774dce51d82d45287
Matias <mati.dehoyosmdp@gmail.com>
2024-07-06 17:33:28 -0300

    }

--- rugir-main/client/src/components/contact/Contact.jsx
+++ rugir-main/client/src/components/contact/Contact.jsx
@@ -14,7 +14,7 @@ function Contact() {
         <h3 data-aos="fade-up">Envianos tu consulta</h3>
         <p data-aos="fade-up">Pedí tu presupuesto através de un mensaje directo, responderemos a la brevedad...</p>
         <div className={style.redes}>
-            <button data-aos="fade-up-right"><a target="_blank" href='https://www.instagram.com/rugirmdq/'><FaInstagram className={style.icon}/>Mensaje</a> </button>
+            <button data-aos="fade-up-right"><a target="_blank" href='https://www.instagram.com/rugirmdq/'><FaInstagram className={style.icon}/>Instagram</a> </button>
             <button data-aos="fade-up-left"><a href="https://api.whatsapp.com/send?phone=542236351363" target="_blank" rel="noopener noreferrer"><FaWhatsapp className={style.icon}/>WhatsApp</a></button>
         </div>
     </div>
