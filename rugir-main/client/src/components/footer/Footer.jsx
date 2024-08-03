e2c3c18d956df23be50e4c9e18a968cec304538f
Matias <mati.dehoyosmdp@gmail.com>
2024-08-03 12:19:06 -0300

    footer

--- rugir-main/client/src/components/footer/Footer.jsx
+++ rugir-main/client/src/components/footer/Footer.jsx
@@ -3,8 +3,8 @@ import style from './Footer.module.css'
 function Footer() {
   return (
     <div className={style.container}>
-        <p className={style.copy}>© 2024 Rugir Mdq. Todos los derechos reservados.
-        </p> 
+        <p className={style.dev}>Desarrollado por <a href="https://mododigital.vercel.app" target="_blank" rel="noopener noreferrer"><span className={style.modo}>MODO</span><span className={style.digi}>DIGITAL</span></a></p>
+        <p className={style.copy}>© 2024 Rugir Mdq. Todos los derechos reservados.</p> 
     </div>
   )
 }
