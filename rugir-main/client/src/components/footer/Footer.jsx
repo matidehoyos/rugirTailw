f1e503d6b8f69cebafe3d297ad05935beb652447
Matias <mati.dehoyosmdp@gmail.com>
2024-08-05 10:18:08 -0300

    end

--- rugir-main/client/src/components/footer/Footer.jsx
+++ rugir-main/client/src/components/footer/Footer.jsx
@@ -3,6 +3,9 @@ import style from './Footer.module.css'
 function Footer() {
   return (
     <div className={style.container}>
+        <div className={style.log}>
+          <img src="./l.png" alt="logo Rugir" />
+        </div>
         <p className={style.dev}>Desarrollado por <a href="https://mododigital.vercel.app" target="_blank" rel="noopener noreferrer"><span className={style.modo}>MODO</span><span className={style.digi}>DIGITAL</span></a></p>
         <p className={style.copy}>© 2024 Rugir Mdq. Todos los derechos reservados.</p> 
     </div>
