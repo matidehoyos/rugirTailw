baf4fff1d0aded1d461ca6d6b4834961b860ef1d
Matias <mati.dehoyosmdp@gmail.com>
2024-08-06 11:11:06 -0300

    cb

--- rugir-main/client/src/components/header/Header.jsx
+++ rugir-main/client/src/components/header/Header.jsx
@@ -20,11 +20,7 @@ function Header() {
                 <div className={style.caja}>
                     <div className={style.first}>
                         <div className={style.titular}>
-                         <div className={style.logo}  data-aos="zoom-out" data-aos-duration="1000">
-
-                            </div> 
                             <h1 data-aos="fade-up" data-aos-duration="1000"  className={style.titulo}>TUFTING RUGS <br/> ALFOMBRAS<br/> TAPETES</h1>
-                       {/*}     <p data-aos="fade-up"  data-aos-duration="1000"  className={style.amano}>PRODUCTOS HECHOS A MANO</p> */}
                             <h4 data-aos="fade-up"  data-aos-duration="1000"  className={style.personalizados}>DISEÑOS PERSONALIZADOS</h4>
                         </div>
                         <div className={style.mov}>
